f375e0be3752959e575e8b402a0d728ebfdd40c7
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-03-27 14:07:20 -0400

    enable cors

--- index.js
+++ index.js
@@ -10,8 +10,26 @@ const client = new Twitter({
 	bearer_token: process.env.TWITTER_BEARER_TOKEN
 });
 
+app.use(function(req, res, next) {
+	var allowedOrigins = ['http://thecreativecrypto.com', 'http://localhost:8080'];
+	var origin = req.headers.origin;
+	if(allowedOrigins.indexOf(origin) > -1){
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+	//res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+	res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.header('Access-Control-Allow-Credentials', true);
+	return next();
+});
+
 App.get('/', (req, res) => {
-	client.get('statuses/user_timeline', {screen_name: 'creative_crypto', tweet_mode: 'extended'}, function(error, tweets, response) {
+	const params = {
+		screen_name: 'creative_crypto',
+		tweet_mode: 'extended'
+	};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if(error) throw error;
 		res.send(tweets);
 	});
